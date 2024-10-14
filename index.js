5a4777b16405bca13d8c29d86b4e0c6d60ee3403
r03 <r03r000@gmail.com>
2024-10-14 11:47:07 +0200

    lo rrecupere de replit

---
+++ index.js
@@ -0,0 +1,48 @@
+// sample array of students abc quick
+
+
+// starin from 1 just Cause a var b= b+1
+
+
+// students.forEach((a, b, c) => {
+//   b += 1
+//   console.log(b, a)
+//})
+const students = [
+  {
+    name: "Jill",
+    lastname: "Uno",
+    age: 24,
+    course: "Marketing",
+  },
+  {
+    name: "John",
+    lastname: "Dos",
+    age: 22,
+    course: "Development",
+  },
+  {
+    name: "Jack",
+    lastname: "tre",
+    age: 20,
+    course: "Accounting",
+  },
+  {
+    name: "Ryan",
+    lastname: "Ray",
+    age: 23,
+    course: "Development",
+  },
+  {
+    name: "Jane",
+    lastname: "cin",
+    age: 20,
+    course: "Management",
+  },
+];
+
+students.forEach((a, b, c) => {
+  b += 1
+  console.log(b, a)
+})
+
